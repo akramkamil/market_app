d9d10835340ce2c28b5f06ca3d624e00b457bdb3
akram kamil <akrambean@gmail.com>
2023-06-13 00:58:41 +0300

    make the UI and solve null safety

---
+++ lib/core/model/ProductSize.dart
@@ -0,0 +1,14 @@
+
+class ProductSize {
+  String size;
+  String name;
+
+  ProductSize({required this.size, required this.name});
+
+  factory ProductSize.fromJson(Map<String, dynamic> json) {
+    return ProductSize(
+      name: json['name'],
+      size: json['size'],
+    );
+  }
+}
